f95d477ab735dd99e2a12a96ee998a1076840c07
Ryan Manchester <ryanm17@gmail.com>
2018-04-25 23:44:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -59,7 +59,8 @@ class Dog
 
   def self.find_by_name(name)
     sql = "SELECT * FROM dogs WHERE name = ?"
-    row = DB[:conn].execute(sql, name)
+    row = DB[:conn].execute(sql, name)[0]
+    Dog.new(id: row[0], name: row[1], breed: row[2])
   end
 
   def save
