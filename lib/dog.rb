83477d23704096d8748c5884385a33a0251e74fd
Ryan Manchester <ryanm17@gmail.com>
2018-04-25 23:35:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -44,6 +44,12 @@ class Dog
     sql = "SELECT * FROM dogs WHERE name = ? AND breed = ?"
     binding.pry
     dog = DB[:conn].execute(sql, hash[:name], hash[:breed])
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+    else
+      dog = self.create(hash)
+    end
 
   end
 
