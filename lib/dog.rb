7edba8906edde8c97d0a0e7727360f2a917be635
Ryan Manchester <ryanm17@gmail.com>
2018-04-25 23:20:45 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -39,7 +39,7 @@ class Dog
 binding.pry
     result = DB[:conn].execute(sql, id)[0]
 
-    Dog.create(id: result[0], name: result[1], breed: result[2])
+    Dog.new(id: result[0], name: result[1], breed: result[2])
 
   end
 
