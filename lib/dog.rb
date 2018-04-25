c16608e9c71769cd6ae7462c4e2812b9d084a1c5
Ryan Manchester <ryanm17@gmail.com>
2018-04-25 22:46:09 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -31,11 +31,12 @@ class Dog
   end
 
   def self.find_by_id(id)
-    binding.pry
+
     sql = <<-SQL
     SELECT * FROM dogs
     WHERE id = ?
     SQL
+    binding.pry
     DB[:conn].execute(sql, id)
   end
 
