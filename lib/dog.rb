a5963febedad625c5e142b21dd5cd6c10a3e316c
Ryan Manchester <ryanm17@gmail.com>
2018-04-25 22:15:29 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -25,8 +25,16 @@ class Dog
   end
 
   def save
-    new_dog = Dog.new(name:, breed:)
-    new_dog
+    if self.id
+      self.update
+    else
+    sql = <<-SQL
+    INSERT INTO dogs (name, breed)
+    VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+  end
   end
 
 end
