2e59cf44307022dc0dc31cf3b2cf6d39e07a462b
Ryan Manchester <ryanm17@gmail.com>
2018-04-25 22:06:39 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,7 +11,7 @@ class Dog
 
   def self.create_table
     sql = <<-SQL
-    CREATE TABLE dogs (
+    CREATE TABLE IF NOT EXISTS dogs (
       id INTEGER PRIMARY KEY,
       name TEXT,
       breed TEXT
